b28ca1a95ea3334d9ef0cc2cdfbfcd8f5c961b6e
zepplingryo <zeppLinGryo@163.com>
2021-11-13 17:08:08 +0800

    js变量执行

--- 01_v8引擎/js变量执行.js
+++ 01_v8引擎/js变量执行.js
@@ -20,8 +20,25 @@ console.log(window.window.window.window.window.window.window.window.window);
  *
  * 2. 运行代码
  *
- * v8为了执行代码 是通过创建一个执行上下文栈方式(函数调用栈实现的) 栈结构 Execution Context Stack 简称 ECStack
+ * v8为了执行代码 是通过创建一个执行上下文栈方式(函数调用栈实现的)
  *
+ * 栈结构 Execution Context Stack 简称 ECStack
+ *
+ * 因为我们执行的是全局代码 为了全局代码能够正常的执行 需要创建 全局执行上下文(全局代码需要被执行时候才会被创建)
+ *
+ * 全局执行上下文(Global Execution Context)
+ *
+ * 全局执行上下文需要维护一个变量 VO(variable Object)
+ *
+ * VO与GO的关系是 VO上的元素都会背映射到GO上
+ *
+ * 一个变量预解析以后变为undfined
+ *
+ * 而此时代码执行时会将变量值赋值 同时VO会将修改的值映射到GO中
+ *
+ * 所以说变量提升相关的东西就非常好解释了 如果在变量未赋值前就执行 那肯定先去找GO中的undfined
+ *
+ * 现在再想作用域提升 好像只是全局变量GO扫码js代码后进行收集
  */
 
 var redux = "redux";
