e6e532856b5552e3bcd0dadbd707eda1610633e9
zepplingryo <zeppLinGryo@163.com>
2021-11-14 15:42:39 +0800

    高阶函数

---
+++ 05_函数是一等公民/数组中函数使用.js
@@ -0,0 +1,19 @@
+var arr = [1, 2, 3, 4, 5];
+
+// arr
+//   .filter((ele) => (ele % 2 === 0 ? true : false))
+//   .forEach((r) => console.log(r));
+
+var newArr = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+
+console.log(newArr);
+
+// map
+
+// forEach
+
+// reduce
+
+// find
