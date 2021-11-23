95d6bf643e6282eda32eb6fbd4d78c377441ce67
zepplingryo <zeppLinGryo@163.com>
2021-11-23 21:59:50 +0800

    slice的实现

---
+++ 09_函数式编程/js内置高阶api/slice.js
@@ -0,0 +1,15 @@
+Array.prototype._slice = function (start, end) {
+  var arr = this;
+
+  var newArray = [];
+
+  for (var i = start; i < end; i++) {
+    newArray.push(arr[i]);
+  }
+
+  return newArray;
+};
+
+// 这里结合arguments一起来看
+var newArray = Array.prototype._slice.call([1, 2, 3], 1, 3);
+console.log(newArray);
--- 10_严格模式&eval&类数组对象/arguments.js
+++ 10_严格模式&eval&类数组对象/arguments.js
@@ -5,3 +5,26 @@
  *
  * arguments会被存储在 变量对象AO中
  */
+function foo() {
+  /**
+   * 常见的arguments操作
+   * 1. 获取参数的长度
+   * 2. 根据索引值获取某一个参数
+   * 3. callee获取当前argumrnts所在的函数 这个别瞎用
+   */
+  console.log(arguments[0]);
+  console.log(arguments.length);
+  console.log(arguments.callee);
+
+  // 类数组转数组
+  var newArr = Array.prototype.slice.call(arguments);
+  // var newArr = [].slice.call(arguments) // 这个两个就能看到显示绑定和隐式绑定的效果了
+  console.log(newArr);
+}
+
+const bar = () => {
+  console.log(arguments);
+};
+
+foo(1, 2, 3, 4);
+bar(1, 2, 3, 4);
